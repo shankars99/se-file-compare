15614bbea20791d02fcfea448eb0b31260d39fa2
shankars99 <shankars0899@gmail.com>
2020-05-18 19:43:52 +0530

    Reading languages from file done

--- languages/Language.java
+++ languages/Language.java
@@ -1,25 +1,47 @@
 package languages;
 
 import mapper.Mapper;
+import java.util.*;
+import java.io.*;
 
 public class Language extends Mapper {
     /*
      * Get the all the keys associated with given Value V from map
      */
+    String str = "";
+    String fname = "";
+
+    public void setPath(final String s) {
+        str = "";
+        fname = s;
+
+        str = "languages/languagelist" + s;
+    }
+
+    public String getFileName()
+    {
+        return fname;
+    }
+
     @Override
-    public String getString() {
-        final String fileImport = "java : Java, c : C, cpp : C++, lex : C, js : JavaScript, jsm : JavaScript";
-        return fileImport;
+    public String readWords() throws IOException {
+        String line = "";
+        str = str + ".txt";
+        final Scanner scanner = new Scanner(new File(str));
+        while (scanner.hasNext()) {
+            line = line + scanner.next();
+        }
+        return line;
     }
 
     @Override
     public String[][] splitter(final String s) {
 
-        final String temp[] = s.split(", ");
+        final String temp[] = s.split(",");
         final String str[][] = new String[temp.length][2];
 
         for (int i = 0; i < str.length; i++) {
-            str[i] = temp[i].split(" : ");
+            str[i] = temp[i].split(":");
             str[i][0] = "." + str[i][0];
         }
 
