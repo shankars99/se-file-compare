df2d1fc3a91050618136341a20e9ec18ceae8baf
shankars99 <shankars0899@gmail.com>
2020-05-18 19:44:30 +0530

    Add comparision

--- CompareLang.java
+++ CompareLang.java
@@ -1,21 +1,32 @@
 import languages.Language;
 
+import java.io.*;
 import java.util.*;
 
 public class CompareLang {
-    public static void main(final String[] args) {
-         final Language obj = new Language();
 
-         // Create a Map of words and their frequency count
-         final String str[][] = obj.splitter(obj.getString());
-         final HashMap<String, String> mapOfExtension = obj.getMap(str);
-         final String testKey = "JavaScript";
+    public static void readFiles(final Language obj) throws IOException {
+        // Create a Map of words and their frequency count
+        final String str[][] = obj.splitter(obj.readWords());
+        final HashMap<String, String> mapOfExtension = obj.getMap(str);
+        final String testKey = "JavaScript";
 
-         // Get the list of keys whose value matches with given value.
-         final List<String> listOfKeys = obj.getKeys(mapOfExtension, testKey);
+        // Get the list of keys whose value matches with given value.
+        final List<String> listOfKeys = obj.getKeys(mapOfExtension, testKey);
 
+        System.out.println(obj.getFileName());
         System.out.println(listOfKeys);
-        System.out.println("\n" + testKey + " = " + mapOfExtension.get(".js"));
+        System.out.println("" + testKey + " = " + mapOfExtension.get(".js") + "\n");
+    }
+
+    public static void main(final String[] args) throws IOException {
+        final Language obj = new Language();
+
+        obj.setPath("Gedit");
+        readFiles(obj);
+
+        obj.setPath("Notepad++");
+        readFiles(obj);
     }
 
 }
